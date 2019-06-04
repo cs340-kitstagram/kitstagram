62141c76d372b7375f1b1cc7465256f41acecb7b
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2019-06-03 17:01:53 -0700

    fix foreign key constraints
    MySQL doesn't seem to recognize the short form

--- db.sql
+++ db.sql
@@ -13,28 +13,35 @@ CREATE TABLE Cats (
 ) ENGINE=InnoDB COLLATE utf8mb4_general_ci;
 
 CREATE TABLE Friends (
-    friender_id INTEGER NOT NULL REFERENCES Cats (id),
-    friendee_id INTEGER NOT NULL REFERENCES Cats (id),
+    friender_id INTEGER NOT NULL,
+    friendee_id INTEGER NOT NULL,
+    FOREIGN KEY(friender_id) REFERENCES Cats (id),
+    FOREIGN KEY(friendee_id) REFERENCES Cats (id),
     PRIMARY KEY(friender_id, friendee_id)
 ) ENGINE=InnoDB COLLATE utf8mb4_general_ci;
 
 CREATE TABLE Selfies (
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
     date_uploaded TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    cat_id INTEGER NOT NULL REFERENCES Cats(id)
+    cat_id INTEGER NOT NULL REFERENCES Cats(id),
+    FOREIGN KEY(cat_id) REFERENCES Cats(id)
 ) ENGINE=InnoDB COLLATE utf8mb4_general_ci;
 
 CREATE TABLE Likes (
-    cat_id INTEGER REFERENCES Cats(id),
-    selfie_id INTEGER REFERENCES Selfies(id),
+    cat_id INTEGER NOT NULL,
+    selfie_id INTEGER NOT NULL,
+    FOREIGN KEY(cat_id) REFERENCES Cats(id),
+    FOREIGN KEY(selfie_id) REFERENCES Selfies(id),
     PRIMARY KEY(cat_id, selfie_id)
 ) ENGINE=InnoDB COLLATE utf8mb4_general_ci;
 
 CREATE TABLE Comments (
-    selfie_id INTEGER REFERENCES Selfies(id),
+    selfie_id INTEGER NOT NULL REFERENCES Selfies(id),
     comment_number INTEGER NOT NULL,
     cat_id INTEGER NOT NULL REFERENCES Cats(id),
     date_posted TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     body TEXT,
+    FOREIGN KEY(selfie_id) REFERENCES Selfies(id),
+    FOREIGN KEY(cat_id) REFERENCES Cats(id),
     PRIMARY KEY(selfie_id, comment_number)
 ) ENGINE=InnoDB COLLATE utf8mb4_general_ci;
