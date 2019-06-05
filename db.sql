066f14ebe5ef2bb08b3f3b00dff8a05dc12c582b
buica <buica@oregonstate.edu>
2019-06-04 23:07:10 -0700

    Update db.sql
    Added 10 initial entries to each table

--- db.sql
+++ db.sql
@@ -63,4 +63,58 @@ CREATE TRIGGER decr_likes
     UPDATE Selfies SET likes = likes - 1 WHERE id = old.selfie_id;
 
 -- password=test
-INSERT INTO Cats VALUES (1, 'zelda', '$2y$10$OIWoxot8isgsxegW2.m/UepN3La0xUhRu/7RmeI034kE8axH.PqXi', 'Zelda', '');
+-- Cats Table Initial Values --
+INSERT INTO Cats VALUES (1, 'zelda', '$2y$10$OIWoxot8isgsxegW2.m/UepN3La0xUhRu/7RmeI034kE8axH.PqXi', 'Zelda', 'add me');
+INSERT INTO Cats VALUES (2, 'link', '$2y$10$RZAUfpa/gazs7yyDmbQtrOusmvvQaeFPsAH2fGWSfDB73K.kw2btO', 'Link', 'I follow back');
+INSERT INTO Cats VALUES (3, 'buster', '$2y$10$.AdP88ICsVj8w1IGszHRnuVk//QWP7btvH37xRD6ofz0/W.IK3jEC', 'Buster', 'A cool cat');
+INSERT INTO Cats VALUES (5, 'misty', '$2y$10$c1O9OvOSvLxmN1zQoCBFgOsbWTeOxzMsUuBSnT79W8gC7IxjvwtW.', 'Misty', 'I dont know how to use this');
+INSERT INTO Cats VALUES (6, 'whisky', '$2y$10$Otd/TL4FMeS4AxNTeF/UoevjLCFvzf9udrnLICdfQPlvYL9OsTrL.', 'Whisky', 'Just another cool cat');
+INSERT INTO Cats VALUES (7, 'sirpounce', '$2y$10$NxfO3B7nPQ9IOyFOigVg/uqTrGm3pipCn5ftHFN5ce5Rwb7vK4h9C', 'Sir Pounce', ':)');
+INSERT INTO Cats VALUES (10, 'tom', '$2y$10$J/zofpDWcIUzPCqlPlZZcOmxhvIsbaUOKJ5/RP8RTUv1kBELUAqZm', 'Tom', 'Im crazy for catnip');
+INSERT INTO Cats VALUES (12, 'arya', '$2y$10$C7ZQchwZIBNJc.ZVr0wSBe/70mnHDvC8u2v9KU1qQxs/7t3MRezhm', 'Arya', '...');
+INSERT INTO Cats VALUES (23, 'lulu', '$2y$10$lKO2NWhJTUsjMl3TWvXEp.50uX867BZelPvvcMiKtk3E713MFJ.I.', 'Lulu', 'Lets be friends');
+INSERT INTO Cats VALUES (33, 'poppy', '$2y$10$0Inka0ZnIR.21LjYmGOA8e14LtCR5U/Ke7HjzexK5mFToNM4BC8Y6', 'Poppy', 'Im Poppy');
+-- Friends Table Initial Values --
+INSERT INTO Friends VALUES (1, 2);
+INSERT INTO Friends VALUES (1, 5);
+INSERT INTO Friends VALUES (1, 6);
+INSERT INTO Friends VALUES (1, 10);
+INSERT INTO Friends VALUES (3, 10);
+INSERT INTO Friends VALUES (3, 6);
+INSERT INTO Friends VALUES (7, 12);
+INSERT INTO Friends VALUES (12, 23);
+INSERT INTO Friends VALUES (5, 23);
+INSERT INTO Friends VALUES (23, 33);
+-- Selfies Table Initial Values --
+INSERT INTO Selfies VALUES (101, 'selfie link', 'me', current_timestamp(), 1, 2);
+INSERT INTO Selfies VALUES (201, 'selfie link2', 'selfie 1', current_timestamp(), 2, 0);
+INSERT INTO Selfies VALUES (202, 'selfie link3', 'selfie 2', current_timestamp(), 2, 2);
+INSERT INTO Selfies VALUES (301, 'selfie link4', 'happy cat', current_timestamp(), 3, 0);
+INSERT INTO Selfies VALUES (501, 'selfie link5', 'misty selfie', current_timestamp(), 5, 2);
+INSERT INTO Selfies VALUES (601, 'selfie link6', 'whisky selfie', current_timestamp(), 6, 1);
+INSERT INTO Selfies VALUES (701, 'selfie link7', 'pouncin', current_timestamp(), 7, 0);
+INSERT INTO Selfies VALUES (1001, 'selfie link8', 'tom selfie', current_timestamp(), 10, 1);
+INSERT INTO Selfies VALUES (1201, 'selfie link9', 'arya selfie', current_timestamp(), 12, 2);
+INSERT INTO Selfies VALUES (2301, 'selfie link0', 'lulu selfie', current_timestamp(), 23, 0);
+-- Likes Table Initial Values --
+INSERT INTO Likes VALUES (1, 101);
+INSERT INTO Likes VALUES (2, 101);
+INSERT INTO Likes VALUES (1, 202);
+INSERT INTO Likes VALUES (3, 501);
+INSERT INTO Likes VALUES (6, 202);
+INSERT INTO Likes VALUES (7, 1001);
+INSERT INTO Likes VALUES (7, 1201);
+INSERT INTO Likes VALUES (12, 501);
+INSERT INTO Likes VALUES (23, 1201);
+INSERT INTO Likes VALUES (33, 601);
+-- Comments Table Initial Values --
+INSERT INTO Comments VALUES (202, 1, 1, current_timestamp(), 'cool pic');
+INSERT INTO Comments VALUES (101, 1, 3, current_timestamp(), 'purrfect');
+INSERT INTO Comments VALUES (101, 2, 6, current_timestamp(), '<3');
+INSERT INTO Comments VALUES (101, 3, 12, current_timestamp(), 'nice');
+INSERT INTO Comments VALUES (1001, 1, 7, current_timestamp(), 'meowzers');
+INSERT INTO Comments VALUES (701, 1, 12, current_timestamp(), 'thats my friend');
+INSERT INTO Comments VALUES (501, 1, 1, current_timestamp(), 'aww');
+INSERT INTO Comments VALUES (2301, 1, 33, current_timestamp(), 'cutie');
+INSERT INTO Comments VALUES (601, 1, 23, current_timestamp(), 'wow');
+INSERT INTO Comments VALUES (202, 2, 12, current_timestamp(), 'your best selfie yet!');
